6ee66f41896067b48d7e74375c7ef4ddcc2af84d
Evgeny Reznichenko <kusakyky@gmail.com>
2016-02-08 18:37:30 +0300

    init

---
+++ example/one/src/index.js
@@ -0,0 +1,23 @@
+/**
+ * @author "Evgeny Reznichenko" <kusakyky@gmail.com>
+ */
+
+
+require.ensure([], function () {
+    require('react');
+}, 'react');
+
+require.ensure([], function () {
+    require('jquery');
+}, 'jquery');
+
+require.ensure([], function () {
+    require('angular');
+}, 'angular');
+
+
+require.ensure([], function () {
+    require('underscore');
+}, 'underscore');
+
+
---
+++ two-webpack.config.js
@@ -0,0 +1,44 @@
+/**
+ * @author "Evgeny Reznichenko" <kusakyky@gmail.com>
+ */
+var
+    webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        index: __dirname + '/example/one/src/index.js'
+    },
+    output: {
+        path: __dirname + '/example/one//output/',
+        filename: '[name].js'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            //see: https://github.com/mishoo/UglifyJS2#compressor-options
+            compressor: {
+                sequences: true,
+                properties: true,
+                dead_code: true,
+                drop_debugger: true,
+                unsafe: false,
+                conditionals: true,
+                comparisons: true,
+                evaluate: true,
+                booleans: true,
+                loops: true,
+                unused: false,
+                hoist_funs: true,
+                hoist_vars: false,
+                if_return: true,
+                join_vars: true,
+                cascade: true,
+                warnings: true,
+                negate_iife: true,
+                pure_getters: false,
+                pure_funcs: null,
+                drop_console: false,
+                keep_fargs: false
+            }
+        })
+    ]
+};
